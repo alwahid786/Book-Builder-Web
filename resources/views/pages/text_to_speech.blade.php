e1dab9e463b11a371957961045346983e0ab8ec1
shoaibanwar <chshoaibanwar3035@gmail.com>
2023-03-14 11:06:17 +0500

    Update text_to_speech.blade.php

--- resources/views/pages/text_to_speech.blade.php
+++ resources/views/pages/text_to_speech.blade.php
@@ -7,7 +7,9 @@
     padding: 10px;
   }
 
-  #startBtn, #stopBtn, #resetBtn {
+  #startBtn,
+  #stopBtn,
+  #resetBtn {
     display: inline-block;
     padding: 10px;
     font-size: 16px;
@@ -17,7 +19,8 @@
     cursor: pointer;
   }
 
-  #stopBtn, #resetBtn {
+  #stopBtn,
+  #resetBtn {
     background-color: #f44336;
   }
 </style>
@@ -28,7 +31,12 @@
 
 <!-- create a text area to display the transcribed text -->
 <textarea id="transcription" rows="5"></textarea>
+<script src="https://cdn.ckeditor.com/4.16.1/standard-all/ckeditor.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 <script>
+  CKEDITOR.replace('transcription', {
+    height: '400px'
+  });
   let recognition;
   let transcription = '';
   const startBtn = document.getElementById('startBtn');
@@ -38,9 +46,9 @@
 
   // create a new instance of SpeechRecognition
   if (window.SpeechRecognition || window.webkitSpeechRecognition) {
-      recognition = new (window.SpeechRecognition || window.webkitSpeechRecognition)();
+    recognition = new(window.SpeechRecognition || window.webkitSpeechRecognition)();
   } else {
-      console.log('Speech recognition not supported');
+    console.log('Speech recognition not supported');
   }
 
   // set recognition properties
@@ -50,39 +58,53 @@
 
   // handle result event
   recognition.onresult = function(event) {
-      let interimTranscription = '';
-      for (let i = event.resultIndex; i < event.results.length; i++) {
-          let transcript = event.results[i][0].transcript;
-          if (event.results[i].isFinal) {
-              transcription += transcript + ' ';
-          } else {
-              interimTranscription += transcript;
-          }
+    let interimTranscription = '';
+    for (let i = event.resultIndex; i < event.results.length; i++) {
+      let transcript = event.results[i][0].transcript;
+      if (event.results[i].isFinal) {
+        var editor = CKEDITOR.instances.transcription;
+
+        // Get the current selection object
+        var selection = editor.getSelection();
+
+        // Get the current element where the cursor is blinking
+        var element = selection.getStartElement();
+
+        // Insert the text at the cursor position
+        // editor.setData('', { selectionStart: element, selectionEnd: element });
+        editor.insertText(transcript, element);
+        // CKEDITOR.instances.transcription.insertHtml(transcript);
+        //   transcription += transcript + ' ';
       }
-      transcriptionField.value = transcription + interimTranscription;
+      //    else {
+      //       interimTranscription += transcript;
+      //   }
+    }
+    //   transcriptionField.value = transcription + interimTranscription;
+
   };
 
   // handle error event
   recognition.onerror = function(event) {
-      console.log('Error occurred in recognition: ' + event.error);
+    console.log('Error occurred in recognition: ' + event.error);
   };
 
   // handle end event
   recognition.onend = function() {
-      console.log('Recognition ended');
-      startBtn.style.display = 'inline-block';
-      resetBtn.style.display = 'inline-block';
-      stopBtn.style.display = 'none';
+    console.log('Recognition ended');
+    startBtn.style.display = 'inline-block';
+    resetBtn.style.display = 'inline-block';
+    stopBtn.style.display = 'none';
   };
 
   // add click event listener to start button
   startBtn.addEventListener('click', function() {
     //   if (transcription === '') {
-          recognition.start();
-          console.log('Recognition started');
-          startBtn.style.display = 'none';
-          resetBtn.style.display = 'none';
-          stopBtn.style.display = 'inline-block';
+    recognition.start();
+    console.log('Recognition started');
+    startBtn.style.display = 'none';
+    resetBtn.style.display = 'none';
+    stopBtn.style.display = 'inline-block';
     //   }
   });
 
@@ -90,21 +112,35 @@
   stopBtn.addEventListener('click', function() {
     //   transcription = '';
     //   transcriptionField.value = '';
-      recognition.stop();
-      console.log('Recognition stopped');
-      startBtn.style.display = 'inline-block';
-          resetBtn.style.display = 'inline-block';
-      stopBtn.style.display = 'none';
+    recognition.stop();
+    console.log('Recognition stopped');
+    startBtn.style.display = 'inline-block';
+    resetBtn.style.display = 'inline-block';
+    stopBtn.style.display = 'none';
   });
 
-  
+
   // add click event listener to reset button
   resetBtn.addEventListener('click', function() {
-      transcription = '';
-      transcriptionField.value = '';
-      recognition.stop();
-      console.log('Recognition stopped');
-      resetBtn.style.display = 'none';
+    Swal.fire({
+  title: 'Are you sure?',
+  text: "You won't be able to revert this!",
+  icon: 'warning',
+  showCancelButton: true,
+  confirmButtonColor: '#3085d6',
+  cancelButtonColor: '#d33',
+  confirmButtonText: 'Yes, reset it!'
+}).then((result) => {
+  if (result.isConfirmed) {
+    // Perform the action here
+   
+    transcription = '';
+    CKEDITOR.instances.transcription.setData('');
+    recognition.stop();
+    console.log('Recognition stopped');
+    resetBtn.style.display = 'none';
+  }
+})
   });
 </script>
 @endsection
