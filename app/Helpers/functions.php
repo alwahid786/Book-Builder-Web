d29bd5c3739b5fb780fe70b90ca0141fed547912
shoaibanwar <chshoaibanwar3035@gmail.com>
2023-03-22 12:09:39 +0500

    Update functions.php

--- app/Helpers/functions.php
+++ app/Helpers/functions.php
@@ -6,40 +6,40 @@ use App\Models\Outline;
 function bookProgress()
 {
     $sections = [
-        'avatar' => true,
-        'book_title' => true,
-        'outline' => true,
-        'cover_art' => true,
-        'inside_cover' => true,
-        'copy_right' => true,
-        'praise' => true,
-        'dedication' => true,
-        'how_to_use' => true,
-        'forword' => true,
-        'table_of_content' => true,
-        'conclusion' => true,
-        'work_with_us' => true,
-        'about' => true,
+        'avatar' => false,
+        'book_title' => false,
+        'outline' => false,
+        'cover_art' => false,
+        'inside_cover' => false,
+        'copy_right' => false,
+        'praise' => false,
+        'dedication' => false,
+        'how_to_use' => false,
+        'forword' => false,
+        'table_of_content' => false,
+        'conclusion' => false,
+        'work_with_us' => false,
+        'about' => false,
     ];
 
-    // $outlines = Outline::where('user_id', auth()->user()->id)->first();
-    // if(!empty($outlines)){
-    //     $sections['outline'] = true;
-    // }
-    // $book_details = Book::where('user_id', auth()->user()->id)->first();
-    // if (!empty($book_details)) {
-    //     if (!empty($book_details->avatar_fname)) {
-    //         $sections['avatar'] = true;
-    //     }
+    $outlines = Outline::where('user_id', auth()->user()->id)->first();
+    if(!empty($outlines)){
+        $sections['outline'] = true;
+    }
+    $book_details = Book::where('user_id', auth()->user()->id)->first();
+    if (!empty($book_details)) {
+        if (!empty($book_details->avatar_fname)) {
+            $sections['avatar'] = true;
+        }
 
-    //     if (!empty($book_details->book_title)) {
-    //         $sections['book_title'] = true;
-    //     }
+        if (!empty($book_details->book_title)) {
+            $sections['book_title'] = true;
+        }
 
-    //     if (!empty($book_details->front_cover)) {
-    //         $sections['cover_art'] = true;
-    //     }
-    // }
+        if (!empty($book_details->front_cover)) {
+            $sections['cover_art'] = true;
+        }
+    }
     $count_true = count(array_filter($sections)); // count number of true values
     $total = count($sections); // count total elements
     $percentage =  round($count_true / $total * 100); // calculate percentage
